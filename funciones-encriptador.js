0b3d0bf694c1ae2bec9f108b054182060f715eeb
Jhonatan <jhonatan.aparicio.4@gmail.com>
2024-03-19 11:27:57 -0500

    Actualizacion del encriptador

--- funciones-encriptador.js
+++ funciones-encriptador.js
@@ -1,3 +1,9 @@
-function saberelvalor() {
-    console.log(document.getElementById("padrasto").value);
+function encriptar() {
+    const textoInput=document.getElementById("padrasto").value;
+    let textoConvertido=textoInput.replaceAll("a", "ai");
+    let textoConvertido2=textoConvertido.replaceAll("e", "enter");
+    let textoConvertido3=textoConvertido2.replaceAll("i", "imes");
+    let textoConvertido4=textoConvertido3.replaceAll("o", "ober");
+    let textoConvertido5=textoConvertido4.replaceAll("u", "ufat");
+    console.log(textoConvertido5);
 }
